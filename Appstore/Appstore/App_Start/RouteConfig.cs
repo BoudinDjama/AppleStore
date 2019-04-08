eae7b2a2ebba3cfaa93dab4528751e324711a8b9
Boudin Ahmed <boudin321@gmail.com>
2019-04-07 23:46:32 -0400

    Routing, 1st app (example)...

--- Appstore/Appstore/App_Start/RouteConfig.cs
+++ Appstore/Appstore/App_Start/RouteConfig.cs
@@ -12,12 +12,39 @@ namespace Appstore
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+         // Le Path pour le home page 
+            routes.MapRoute(
+             name: "Today",
+             url: "AppStore/Today",
+             defaults: new { controller = "AppStore", action = "Today", id = UrlParameter.Optional }
+         );
 
+         // Le Path pour la categorie jeu 
+            routes.MapRoute(
+             name: "Games",
+             url: "AppStore/Games",
+             defaults: new { controller = "AppStore", action = "Games", id = UrlParameter.Optional }
+         );
+         // Le Path pour les applications
+            routes.MapRoute(
+             name: "Apps",
+             url: "AppStore/Apps",
+             defaults: new { controller = "AppStore", action = "Apps", id = UrlParameter.Optional }
+         );
+         // Le Path pour le Search Bar
+            routes.MapRoute(
+               name: "SearchRoute",
+               url: "AppStore/Search/{id}",
+               defaults: new { controller = "AppStore", action = "Search", id = UrlParameter.Optional }
+           );
+
+         // La Page principal, le Path par defaut
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "AppStore", action = "Today", id = UrlParameter.Optional }
             );
+            
         }
     }
 }
---
+++ Appstore/Appstore/Controllers/AppStoreController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Appstore.Controllers
+{
+    public class AppStoreController : Controller
+    {
+        // GET: Apps
+        public ActionResult Search()
+        {
+            return View();
+        }
+        public ActionResult Games()
+        {
+            return View();
+        }
+        public ActionResult Apps()
+        {
+            return View();
+        }
+        public ActionResult Today()
+        {
+            return View();
+        }
+    }
+}
